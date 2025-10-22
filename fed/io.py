88986cabc50f0ea8bafd8982b1c9799721412348
Gil Dexter <gbd2117@columbia.edu>
2025-10-22 12:51:08 +0100

    added helpers + built notebook

---
+++ fed/io.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(fp):
+    df = pd.read_csv(fp, skiprows=3)
+    country_col = 'Country Name'
+    df = df.rename(columns={country_col: "country"})
+    return df
+
+
